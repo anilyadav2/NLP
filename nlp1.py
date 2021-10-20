046c13a2020151a7095bd1077a06671c2ccbc20c
Anil Yadav <anil_yadav2@baylor.edu>
2021-10-20 14:51:25 -0500

    init

---
+++ nlp1.py
@@ -0,0 +1,47 @@
+from textblob import TextBlob
+
+text = "Today is a beautiful day. Tommorow looks like bad weather."
+
+blob=TextBlob(text)
+
+# print(blob.sentences)
+# print(blob.words)
+# print(blob.tags)
+#print(blob.noun_phrases)
+
+# print(round(blob.sentiment.polarity,3))
+# print(round(blob.sentiment.subjectivity,3))
+
+
+# for i in blob.sentences:
+#     print(i,round(i.sentiment.polarity,3))
+
+
+
+
+
+from textblob.sentiments import NaiveBayesAnalyzer
+
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+
+
+# for sentences in blob.sentences:
+#     print(sentences.sentiment)
+
+
+
+spanish=blob.translate(to='es')
+
+chines= blob.translate(to='zh')
+
+print(spanish.translate)
+print(chines.translate)
+
+
+
+
+
+
+# print(blob.sentiment)
+
+
